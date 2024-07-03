4c8ed81f82ecccff08fe4458cdd48cfa8edf8fcc
nsk1501 <nsk1501@naver.com>
2024-07-03 15:45:04 +0900

    Revise Mainpage

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -14,15 +14,15 @@ const MainScreen = () => {
   return (
     <div className="main-screen">
       <div className="donuts">
-        <img src={donutImage} alt="Donuts"/>
+        <img src={donutImage} alt="Donuts" />
       </div>
       <div className="text">
-        <h1>AI추천 데이트코스</h1>
-        <h2>성수데이트</h2>
+        <h1>AI추천<br />데이트코스</h1>
+        <h2>성수데이팅</h2>
         <p>
-          밥 먹고, 카페 가고, 팝업 가고, ...
-          데이트코스를 어디서부터 어떻게 짜야할 지 모르겠다면?
-          AI추천을 활용해서 골라봐!
+          밥 먹고, 카페 가고, 팝업 가고, ...<br />
+          데이트코스를 어디서부터 어떻게 짜야할 지<br />
+          모르겠다면? AI추천을 활용해서 골라봐!<br />
           신개념 데이트코스 메이커, 성수데이트
         </p>
         <button onClick={handleButtonClick}>데이트코스 생성하기</button>
